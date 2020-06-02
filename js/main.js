76e377186788971752df5a4734698c850856399d
AvzTentacion <aviwemhlatyana249@gmail.com>
2020-06-02 16:03:34 +0200

    added first part of the project

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var wadges , expense , total;
+
+function _(x){
+    return document.getElementById(x);
+}
+
+function pay(){
+    wadges = _("payment").value;
+    _("avz") = wadges;
+}
+var avz = console.log("im here");
